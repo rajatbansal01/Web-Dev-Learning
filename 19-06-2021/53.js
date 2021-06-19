f0c71ff7e2efd58d77b06d10c5c5450ce1dacbd2
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2021-06-19 12:16:35 +0530

    JS

---
+++ 19-06-2021/53.js
@@ -0,0 +1,29 @@
+let a = 23;
+console.log(a);
+console.log("HI I am Rajat Bansal");
+
+
+let name1 = "Rajat";
+let name2 = "Rohan";
+let name3 = "Shubham";
+let greetText = "Good Morning";
+console.log(name1 + "is a good person");
+console.log(name2 + "is a good person");
+
+function greet(name, greetTExt){
+    let name1 = "Hello";
+    console.log(name+"is a good person");
+    console.log(greetText);
+}
+greet(name1,greetText);
+
+function sum(a,b,c){
+    return(a+b+c);
+
+}
+
+console.log(sum(1,2,3));
+
+
+
+
--- 19-06-2021/tut53.js
+++
@@ -1,3 +0,0 @@
-let a = 23;
-console.log(a);
-console.log("HI I am Rajat Bansal")
