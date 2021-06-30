39173e633998e627cb7fca4d4b30cb8f43d1b5dc
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2021-06-30 22:53:44 +0530

    custom module in JS

---
+++ 30-06-2021/68/index.js
@@ -0,0 +1,7 @@
+console.log("this is index.js");
+// const average = require("./mod");
+const mod = require("./mod");
+// const mod = require("./mod");
+console.log(mod.avg([2,3]))
+console.log(mod.name)
+console.log(mod.role)
---
+++ 30-06-2021/68/mod.js
@@ -0,0 +1,15 @@
+function average(arr){
+    let sum = 0;
+    arr.forEach(element=>{
+        sum+=element;
+    });
+    return sum/arr.length;
+}
+
+module.exports = {
+    avg: average,
+    name: "Rajat Bansal", 
+    role : "founder"
+}
+
+// module.exports.name = "Rajat Bansal";  we can use like this also
