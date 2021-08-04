06a42c32ae0b4ce195a19f4be159d47a5aa0bcb3
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2021-08-04 23:26:00 +0530

    backend with node express

--- 03-08-2021/express/app.js
+++ 03-08-2021/express/app.js
@@ -1,24 +1,40 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 80;
-const path = require('path');
 
-// EXPRESS RELATED STUFF
-app.use('/static', express.static('static'));
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {'title': 'PubG is the best game', "content": con}
+    res.status(200).render('index.pug', params);
+})
+
+app.post('/', (req, res)=>{
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+
+    let outputToWrite = `the name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+
+})
 
 
-// PUG Specific Stuff
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-
-// END points
-app.get('/', (req, res) => {
-    res.status(200).render('index.pug');
-});
-
-
-
-// Start the server
+// START THE SERVER
 app.listen(port, ()=>{
-    console.log("App is running on port: " + port);
+    console.log(`The application started successfully on port ${port}`);
 });
